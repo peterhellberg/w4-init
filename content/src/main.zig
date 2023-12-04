aa8f36ef57b5746c38ebac58ba37dda6de747899
Peter Hellberg <peter@c7.se>
2023-12-04 19:54:28 +0100

    content: Cart struct

--- content/src/main.zig
+++ content/src/main.zig
@@ -1,33 +1,20 @@
 const w4 = @import("w4");
 
-var mouse = w4.Mouse{};
-var button = w4.Button{};
+const Cart = struct {
+    fn start(_: *Cart) void {
+        w4.palette(.{ 0x1e1e3c, 0x50506e, 0xbebee6, 0xe6e6ff });
+    }
+    fn update(_: *Cart) void {}
+    fn draw(_: *Cart) void {}
+};
 
-fn input() void {
-    mouse.update();
-    button.update();
-}
-
-fn draw() void {
-    w4.clear(2);
-    w4.line(w4.SCREEN_SIZE, 0, mouse.x, mouse.y);
-    w4.color(0x41);
-    w4.circle(mouse.x, mouse.y, 6);
-    w4.text("Hello, World!", 8, 10);
-}
+var cart = Cart{};
 
 export fn start() void {
-    // Tangerine Noir Palette
-    // https://lospec.com/palette-list/tangerine-noir
-    w4.palette(.{
-        0xfcfcfc, // White
-        0x393541, // Gray
-        0x191a1f, // Black
-        0xee964b, // Orange
-    });
+    cart.start();
 }
 
 export fn update() void {
-    input();
-    draw();
+    cart.update();
+    cart.draw();
 }
