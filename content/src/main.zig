0deea1cec7f75829a4a42cf4254f5b9ddc9ab3c8
Peter Hellberg <peter@c7.se>
2025-09-07 01:15:18 +0200

    content: Update main.zig

--- content/src/main.zig
+++ content/src/main.zig
@@ -1,20 +1,28 @@
 const w4 = @import("w4");
 
-const Cart = struct {
-    fn start(_: *Cart) void {
-        w4.palette(.{ 0x1e1e3c, 0x50506e, 0xbebee6, 0xe6e6ff });
-    }
-    fn update(_: *Cart) void {}
-    fn draw(_: *Cart) void {}
-};
-
-var cart = Cart{};
+var mouse: w4.Mouse = .{};
+var button: w4.Button = .{};
 
 export fn start() void {
-    cart.start();
+    w4.palette(.{ 0x271d2c, 0x7b6960, 0xfff8ed, 0xe01f3f });
 }
 
 export fn update() void {
-    cart.update();
-    cart.draw();
+    mouse.update();
+    button.update();
+
+    draw();
+}
+
+fn input() void {}
+
+fn draw() void {
+    for ([4]w4.Box{
+        .init(0, 0, 160, 40),
+        .init(0, 40, 160, 40),
+        .init(0, 80, 160, 40),
+        .init(0, 120, 160, 40),
+    }, 0..) |box, i| {
+        box.fill(@intCast(i + 1));
+    }
 }
